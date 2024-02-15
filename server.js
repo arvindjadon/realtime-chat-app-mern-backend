dceb15582d243ede705aae92be3796f7fd7dc34a
Arvind <arvindjadon2020@gmail.com>
2024-02-15 10:46:22 +0530

    Add a room id or name to distinguish messages between rooms

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // importing
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./dbMessages";
 
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
 
 // middleware
+app.use(express.json());
 
 // DB config
 const connection_url =
@@ -22,6 +24,29 @@ mongoose.connect(connection_url, {
 
 // api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+// app.post("/api/v1/messages/new", (req, res) => {
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
 // listen
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
